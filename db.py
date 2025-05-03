4b25e0247cec1dfa04032ce75cd5c12bbde014e4
Mirodil33 <mirodilmirjamolov3@gmail.com>
2025-05-03 13:04:54 +0500

    Mening birinchi commit im

---
+++ db.py
@@ -0,0 +1,15 @@
+yer = {
+    "sayyora_nomi": "Yer",
+    "sayyora_joylashuvi": "3-o'rin",
+    "sayyora_haqida": "Yer yagoma bialogik hayot mavjud bo'lgan sayyora"
+}
+
+mars = {
+    "sayyora_nomi": "Mars",
+    "sayyora_joylashuvi": "4-o'rin",
+    "sayyora_haqida": "Mars qizil sayyora"
+}
+
+sayyoralar = [yer, mars]
+
+# print(sayyoralar)
